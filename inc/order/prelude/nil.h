ffe4efe8401a7a63a35ba541542ebc619f79c38e
vesa_karvonen <vesa_karvonen>
2004-06-19 23:04:08 +0000

    8EVAL_NIL -> 8NIL

--- inc/order/prelude/nil.h
+++ inc/order/prelude/nil.h
@@ -6,8 +6,8 @@
  *    Distributed under the Boost Software License, Version 1.0.
  */
 
-#define ORDER_DEF_nil 8EVAL_NIL
+#define ORDER_DEF_nil 8NIL
 
-#define ORDER_CM_8EVAL_NIL(P,e,K,...) ORDER_CM_##K(,,P##__VA_ARGS__)
+#define ORDER_CM_8NIL(P,e,K,...) ORDER_CM_##K(,,P##__VA_ARGS__)
 
 #endif
