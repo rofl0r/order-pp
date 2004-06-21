89c94f3a328159a6809820068b99415b0c368cbb
vesa_karvonen <vesa_karvonen>
2004-06-21 16:13:38 +0000

    War on whitespace

--- inc/order/prelude/emit.h
+++ inc/order/prelude/emit.h
@@ -7,12 +7,12 @@
  */
 
 #define ORDER_PP_DEF_emit ORDER_PP_OP_CM(1,8EMIT)
-#define ORDER_PP_CM_8EMIT(P,x,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__) P##x
+#define ORDER_PP_CM_8EMIT(P,x,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__)P##x
 
 #define ORDER_PP_DEF_emit_expand ORDER_PP_OP_CM(2,8EMIT_EXPAND)
-#define ORDER_PP_CM_8EMIT_EXPAND(P,l,r,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__) P##l P##r
+#define ORDER_PP_CM_8EMIT_EXPAND(P,l,r,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__)P##l P##r
 
-#define ORDER_PP_DEF_emit_paste ORDER_PP_OP_CM(2,8EMIT_PASTE)
-#define ORDER_PP_CM_8EMIT_PASTE(P,l,r,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__) l##r
+#define ORDER_PP_DEF_emit_cat ORDER_PP_OP_CM(2,8EMIT_CAT)
+#define ORDER_PP_CM_8EMIT_CAT(P,l,r,K,...) ORDER_PP_CM_##K(,,P##__VA_ARGS__)l##r
 
 #endif
