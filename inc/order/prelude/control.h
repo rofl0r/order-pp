6f213dc62a6b56248a4ede8458a0d5c71d8e927f
vesa_karvonen <vesa_karvonen>
2004-06-26 10:39:13 +0000

    8exit and 8with_assert

--- inc/order/prelude/control.h
+++ inc/order/prelude/control.h
@@ -6,8 +6,12 @@
  *    Distributed under the Boost Software License, Version 1.0.
  */
 
-#define ORDER_PP_DEF_8when(t,...) 8EVAL_IF,t,8do(__VA_ARGS__),8nil
-#define ORDER_PP_DEF_8unless(t,...) 8EVAL_IF,t,8nil,8do(__VA_ARGS__)
+#define ORDER_PP_DEF_8when(cond,...) 8EVAL_IF,cond,8do(__VA_ARGS__),8nil
+#define ORDER_PP_DEF_8unless(cond,...) 8EVAL_IF,cond,8nil,8do(__VA_ARGS__)
+
+#define ORDER_PP_DEF_8exit ORDER_PP_FN_CM(1,8STOP)
+
+#define ORDER_PP_DEF_8with_assert(cond,body) ORDER_PP_MACRO(8if(cond,body,8exit(8quote(8Assert_Failed(8with_assert(cond,body))))))
 
 #define ORDER_PP_DEF_8for_each_in_range ORDER_PP_FN_CM(3,8PASS_NIL_4,8FOR_EACH_IN_RANGE_4)
 #define ORDER_PP_CM_8FOR_EACH_IN_RANGE_4(P,_,f,lo,hi,...) (,ORDER_PP_EQUAL(,P##lo,P##hi)(,(,),P##lo,ORDER_PP_OPEN f##P,8FOR_EACH_IN_RANGE_4,P##f,ORDER_PP_INC_##lo,P##hi),P##__VA_ARGS__)
