749969a1f77ced04bc9cd40476d2b7e1c99b32ac
vesa_karvonen <vesa_karvonen>
2004-06-27 19:47:38 +0000

    Fix

--- example/is_function.hpp
+++ example/is_function.hpp
@@ -13,7 +13,7 @@
 
 #define GEN_is_function_tester(n)                                               \
 template<class R ORDER_PP(8emit_trailing_params(n, 8quote(class A))) >          \
-yes_type is_function_tester(R (*)(ORDER_PP(8emit_params(n, 8quote(class A)))));
+yes_type is_function_tester(R (*)(ORDER_PP(8emit_params(n, 8quote(A)))));
 
 ORDER_PP(8for_each_in_range(8fn(8I,
                                 8emit(8quote(GEN_is_function_tester),
