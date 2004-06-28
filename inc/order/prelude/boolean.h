c3bfcbbd7f4abab57c3f2d8b9390fb448ac4a36c
vesa_karvonen <vesa_karvonen>
2004-06-28 14:55:39 +0000

    Starting reference documentation

--- inc/order/prelude/boolean.h
+++ inc/order/prelude/boolean.h
@@ -5,12 +5,18 @@
 //
 //    Distributed under the Boost Software License, Version 1.0.
 
+// Boolean true.
 #define ORDER_PP_DEF_8true 8DEF_CONST,8true
 #define ORDER_PP_SYM_8true_8true(P,v) ,P##v,
 
+// Boolean false.
 #define ORDER_PP_DEF_8false 8DEF_CONST,8false
 #define ORDER_PP_SYM_8false_8false(P,v) ,P##v,
 
+// Logical negation.
+//
+//   8not(8true) ==> 8false
+//   8not(8false) ==> 8true
 #define ORDER_PP_DEF_8not ORDER_PP_FN_CM(1,8NOT)
 #define ORDER_PP_CM_8NOT(P,b,...) (,ORDER_PP_CM_8NOT_##b,P##__VA_ARGS__)
 #define ORDER_PP_CM_8NOT_8true 8false
--- inc/order/prelude/fun.h
+++ inc/order/prelude/fun.h
@@ -5,12 +5,15 @@
 //
 //    Distributed under the Boost Software License, Version 1.0.
 
+// Function composition.
 #define ORDER_PP_DEF_8chain ORDER_PP_OP_LEFT_CM(8CHAIN)
 #define ORDER_PP_CM_8CHAIN(P,f,g,...) (,(,ORDER_PP_OPEN f##P,ORDER_PP_OPEN g##P),P##__VA_ARGS__)
 
+// The identity function.
 #define ORDER_PP_DEF_8identity ORDER_PP_FN_CM(1,8IDENTITY)
 #define ORDER_PP_CM_8IDENTITY(P,x,...) (,P##x,P##__VA_ARGS__)
 
+// Ignores the actual parameter and evaluates to nil.
 #define ORDER_PP_DEF_8ignore ORDER_PP_FN_CM(1,8IGNORE)
 #define ORDER_PP_CM_8IGNORE(P,x,...) (,,P##__VA_ARGS__)
 
