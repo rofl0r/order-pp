f84cabcfab8e49db1c301d9aa46fc089bfcfcb55
vesa_karvonen <vesa_karvonen>
2004-07-01 21:12:04 +0000

    Fix(?)

--- test/test-defs.h
+++ test/test-defs.h
@@ -10,6 +10,11 @@
 #define GEN_dim_2(x) (2,x)ORDER_PP(8for_each_in_range(8fn(8X,8emit(8quote(GEN_dim_3),8tuple(8X))),1,3))
 #define GEN_dim_3(x) (3,x)
 
+#define GEN_dimr_0(x) (0,x)ORDER_PP(8rout(8for_each_in_range(8fn(8X,8emit(8quote(GEN_dimr_1),8tuple(8X))),1,3)))
+#define GEN_dimr_1(x) (1,x)ORDER_PP(8rout(8for_each_in_range(8fn(8X,8emit(8quote(GEN_dimr_2),8tuple(8X))),1,3)))
+#define GEN_dimr_2(x) (2,x)ORDER_PP(8rout(8for_each_in_range(8fn(8X,8emit(8quote(GEN_dimr_3),8tuple(8X))),1,3)))
+#define GEN_dimr_3(x) (3,x)
+
 #define ORDER_PP_DEF_8id ORDER_PP_FN(8fn(8X,8X))
 #define ORDER_PP_DEF_1st ORDER_PP_FN(8fn(8X,,8X))
 #define ORDER_PP_DEF_2nd ORDER_PP_FN(8fn(,8Y,8Y))
