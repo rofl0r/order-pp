bceda5ec5b5758ac60ee098a6ba02e4707bcfb8c
vesa_karvonen <vesa_karvonen>
2004-07-02 21:41:28 +0000

    Using GC

--- example/lambda/checked_malloc.c
+++ example/lambda/checked_malloc.c
@@ -4,12 +4,21 @@
 
 #include "checked_malloc.h"
 #include "error.h"
+#if !defined(CHECKED_MALLOC_NO_GC)
+#include <gc/gc.h>
+#else
 #include <malloc.h>
+#define GC_MALLOC malloc
+#endif
 
 void* checked_malloc(ptrdiff_t size) {
+  static _Bool inited = 0;
+  if (!inited)
+    GC_INIT();
+
   if (size < 0)
     ERROR_exit("Negative size of %td bytes specified to checked_malloc.", size);
-  void* result = malloc(size);
+  void* result = GC_MALLOC(size);
   if (!result)
     ERROR_exit("Allocation of %td bytes failed.", size);
   return result;
--- example/lambda/checked_malloc.h
+++ example/lambda/checked_malloc.h
@@ -7,9 +7,9 @@
 
 #include <stddef.h>
 
-// `checked_malloc(size)' allocates a new block of memory of `size' bytes.
-// If `size' is negative or the allocation fails, the program will be
-// exited with an error message.
+// `checked_malloc(size)' allocates a new block of memory of `size' bytes
+// from a garbage collected heap. If `size' is negative or the allocation
+// fails, the program will be exited with an error message.
 extern void* checked_malloc(ptrdiff_t size);
 
 #endif
