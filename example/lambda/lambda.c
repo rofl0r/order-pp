f5e4e947f339802ec4abfec9a3d1a3ba21faadf9
vesa_karvonen <vesa_karvonen>
2004-07-04 12:03:13 +0000

    Documentation

--- example/lambda/lambda.c
+++ example/lambda/lambda.c
@@ -11,36 +11,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// ## An evaluator for the lambda calculus
+// ## An interpreter for the lambda calculus
 //
 // We will now implement a simple interpreter for the lambda
 // calculus using the variant record generator ("datatype.h") and
 // the backtracking recursive descent parser generator
 // ("brd_parser.h"). We will discuss the generators here only
-// briefly. The reader should already be familiar with them.
+// briefly. The reader should refer to their documentation for
+// detailed description of their design and usage. We will also not
+// introduce lambda calculus here, but assume that the reader is
+// sufficiently familiar with it.
 //
-// ### Representation of programs
+// ### The representation of programs
 //
 // Let's start by defining the most important data types used in the
 // interpreter.
 //
 // We will represent identifiers using interned constant strings, as
 // defined by the `str'-module ("str.h" and "str.c"). The main
-// advantage of the representation is that it makes it very
+// advantage of the representation is that it makes it very easy and
 // efficient to compare identifiers for equality.
 
 typedef str_type id_type;
 
 // Like many language processors do, our intepreter uses an abstract
-// syntax tree representation for programs. A natural way to
+// syntax tree representation of programs. A natural way to
 // represent abstract syntax trees is to use variant records. In our
-// little language, a program consist of a list of definitions and a
-// single lambda calculus expression to evaluate. A definition
+// little language, a program consist of a list of definitions, a
+// rather minimal syntactic extension to pure lambda calculus
+// intended to simplify the writing of longer programs, and a single
+// lambda calculus expression to evaluate. A definition simply
 // associates an identifier with the value of an expression.
-// Finally, an expression is either a lambda abstraction, a function
-// application or a variable reference. This informal definition is
-// captured precisely by the following data type definition, which
-// defines a set of three variant records.
+// Finally, a lambda calculus expression is either a lambda
+// abstraction, a function application or a variable reference.
+//
+// The previous informal definition is captured precisely by the
+// following data type definition, which defines a set of three
+// variant records.
 
 DATATYPE_define((prg_type,
                  (Prg_Def,
@@ -67,12 +74,12 @@ DATATYPE_define((prg_type,
 // dependency relation between the types is acyclic, but it is
 // perhaps more natural to define the type of programs first. If a
 // set of variant records has a cyclic dependency relation, then
-// there is no choice, but to define them all at once.
+// there is no choice, they have to be defined all at once.
 //
 // In general, the `DATATYPE_define' macro takes a sequence of
 // variant record type definitions. Each type definition consists of
-// a pair of the type name and the sequence of constructors
-// definitions. Each constructor definitions is a pair consisting of
+// a pair of the type name and a sequence of constructors
+// definitions. Each constructor definition is a pair consisting of
 // the constructor name and a sequence of field types. The
 // `DATATYPE_define' macro then produces definitions of all the
 // variant record types along with constructor functions for all the
@@ -80,10 +87,11 @@ DATATYPE_define((prg_type,
 //
 // ### Unparsing programs
 //
-// In the previous section, we defined the representation of the
+// In the previous section we defined the representation of the
 // abstract syntax of our little language. In this section we will
-// consider the concrete syntax by defining a set of functions that
-// translate an abstract syntax tree to the concrete syntax.
+// consider the concrete syntax of our language by defining a
+// function that translates the abstract syntax tree of an
+// expression to the concrete syntax.
 //
 // First, let's take a look at a BNF description of the concrete
 // syntax we are going to generate. The description is embedded in
@@ -105,21 +113,34 @@ static const str_type lambda_syntax
 // the token `=' separates the identifier and the expression of a
 // definition. Similarly a lambda abstraction begins with the token
 // `\' and a `.' separates the identifier and the expression of a
-// lambda abstraction. Function applications are parenthesized pairs
-// of expression without a puncturator between the expressions.
+// lambda abstraction. Function application is a parenthesized pair
+// of expressions without a puncturator between the expressions.
 //
 // The description of the syntax of identifiers above isn't in BNF,
 // but is rather a regular expression. An identifier must start
 // with a letter or an underscore and can optionally continue with
 // digits, letters or underscores.
 //
-// While not an explicit part of the above syntax description, we
-// will also use whitespace to separate tokens.
+// While not an explicit part of the above syntax description,
+// whitespace separates adjacent tokens that could otherwise be
+// mistaken to be a single token, but whitespace is otherwise
+// ignored.
 //
-// We will then define the unparsing functions that will translate
-// the abstract syntax to the concrete syntax described above. The
-// first function we define, `exp_unparse', translates an expression
-// to a string containing the concrete syntax of the expression.
+// The constant `simple_example' contains a simple example of the
+// syntax.
+
+static const str_type simple_example
+= ("  def true = \\t.\\e.t\n"
+   "  def false = \\t.\\e.e\n"
+   "  ((true false) true)\n");
+
+// The reader should recognize the definitions of the Church
+// booleans above.
+//
+// For our purpose, which is to be able to print the value of a
+// program. it will be sufficient to be able to translate the
+// abstract syntax of an expression to the above concrete syntax.
+// For this purpose we will define the function `exp_unparse'.
 
 static str_type exp_unparse(exp_type exp) {
   DATATYPE_switch
@@ -137,18 +158,18 @@ static str_type exp_unparse(exp_type exp) {
 }
 
 // The most interesting thing in `exp_unparse' is the use of the
-// `DATATYPE_switch' macro. Everything else is quite obvious.
+// `DATATYPE_switch' macro. Everything else should be quite obvious.
 //
 // The `DATATYPE_switch' macro takes three parameters. The first
 // parameter is the expression whose value is to be deconstructed.
 // The second is the type of the value. The third parameter is a
 // sequence of cases. Each case, represented by a variadic tuple,
-// names a constructor and variables to be bound to the fields of
-// the constructor and ends with the code to be executed if the
-// constructor matches the value. Strictly speaking, it isn't
-// necessary to make the code into a block by surrouding it with
-// braces, but it can makes the code stand out making it easier to
-// read.
+// names a constructor and a sequence variables to be bound to the
+// fields of the constructor followed by a block of code to be
+// executed if the value matches the constructor. Strictly speaking,
+// it isn't necessary to make the code into a block, like is done
+// above, by surrouding it with braces, but it makes the code stand
+// out making it easier to read.
 //
 // The `DATATYPE_switch' macro generates code that evaluates the
 // given expression and examines its value to decide which case to
@@ -160,70 +181,42 @@ static str_type exp_unparse(exp_type exp) {
 // semantics manually.
 //
 // You might also make a note of the use of recursion. Recursion is
-// a natural way to implement algorithms on variant records. While C
-// doesn't require a compiler to implement tail recursion
-// optimizations, a good compiler implements it anyway and the
+// a natural way to implement algorithms on inductive data types.
+// While C doesn't require a compiler to implement tail recursion
+// optimization, a good compiler implements it anyway and the
 // performance cost of using recursion is not prohibitive.
 //
-// Then we'll define a simple function `def_unparse' to translate a
-// definition to a string containing the concrete syntax of a
-// definition.
-
-static str_type def_unparse(def_type def) {
-  DATATYPE_switch
-    (def, def_type,
-     (Def,(var)(body), {
-       return str_cat("def ", var, " = ", exp_unparse(body),
-                      str_end); }));
-}
-
-// The above function simply calls `exp_unparse' to translate the
-// body of a definition to a string.
-//
-// Finally, we will define the function `prg_unparse', which
-// translates a complete program to a string containing the concrete
-// syntax.
-
-static str_type prg_unparse(prg_type prg) {
-  DATATYPE_switch
-    (prg, prg_type,
-     (Prg_Def,(first)(rest), {
-       return str_cat(def_unparse(first), " ", prg_unparse(rest),
-                      str_end); })
-     (Prg_Exp,(last), {
-       return exp_unparse(last); }));
-}
-
 // ### Evaluating programs
 //
 // We will now turn to the evaluation of programs. The simple
 // evaluator we will implement here is based on beta reduction, that
-// is, substitution of values for variables.
+// is, direct substitution of values for variables.
 //
 // We will start by defining the `exp_subst' function, which
 // substitutes a given expression for a given variable in another
 // expression.
 
-static exp_type exp_subst(exp_type with, id_type id, exp_type in) {
+static exp_type exp_subst(exp_type exp, id_type id, exp_type in) {
   DATATYPE_switch
     (in, exp_type,
      (Exp_Lambda,(var)(body), {
        return var == id
          ? in
-         : Exp_Lambda(var, exp_subst(with, id, body)); })
+         : Exp_Lambda(var, exp_subst(exp, id, body)); })
      (Exp_Apply,(lhs)(rhs), {
-       return Exp_Apply(exp_subst(with, id, lhs),
-                        exp_subst(with, id, rhs)); })
+       return Exp_Apply(exp_subst(exp, id, lhs),
+                        exp_subst(exp, id, rhs)); })
      (Exp_Var,(var), {
        return var == id
-          ? with
+          ? exp
           : in; }));
 }
 
 // Then we will define the function `exp_reduce', which reduces, or
 // evaluates, a given expression. Our `exp_reduce' has the strict
 // semantics, meaning that arguments are evaluated before
-// substitution.
+// substitution. We leave it as exercise to change the interpreter
+// to use lazy evaluation.
 
 static exp_type exp_reduce(exp_type exp) {
   DATATYPE_switch
@@ -247,14 +240,25 @@ static exp_type exp_reduce(exp_type exp) {
        ERROR_exit("Unbound variable '%s'.", var); }));
 }
 
+// As you can see above, we handle errors in a very simple way. Upon
+// encoutering an error, we simply exit with a rather uninformative
+// message.
+//
+// Another thing worth pointing out above is that we didn't specify
+// any variables to be bound for the `Exp_Apply' and `Exp_Var'
+// constructors in the inner `DATATYPE_switch'. If we had specified
+// variables to be bound, we would have gotten warnings for not
+// referring to them.
+//
 // The astute reader familiar with lambda calculus and beta
-// reduction might now be thinking that we forgot alpha conversion,
-// that is, renaming of bound variables to avoid incorrect variable
-// capture. We could have handled alpha conversion in `exp_reduce',
-// like is often done in mathematical definitions of beta reduction,
-// but doing so would be inefficient, leading to repeated alpha
-// conversion of the same variables. Instead, we will perform full
-// alpha conversion just once.
+// reduction should now be thinking that we forgot alpha conversion,
+// that is, renaming of bound variables to avoid variable capture.
+// We could have handled alpha conversion in `exp_reduce', like is
+// often done in mathematical definitions of beta reduction, but
+// doing so would be inefficient, leading to repeated alpha
+// conversion of the same expressions. Instead, we will implement
+// full renaming of all binding prior to beta reduction, which is
+// usually more efficient.
 //
 // First we'll define the function `id_fresh' that generates a fresh
 // identifier. We make a point of making the form of the generated
@@ -286,14 +290,19 @@ static exp_type exp_fresh(exp_type exp) {
        return exp; }));
 }
 
-// There is one more thing left to do. Definitions are not a normal
-// part of the lambda calculus and aren't evaluated by our previous
-// functions. For the sake of simplicity and to demonstrate that
-// definitions aren't actually needed, we will simply convert a
-// program into an expression by turning definitions into an
-// expression that applies the body of a definition to a lambda
-// whose variable is the name of the definition and whose body is
-// the rest of the program.
+// The above `exp_fresh' function is still rather inefficient, but
+// it is still usually more efficient than performing alpha
+// conversion repeatedly.
+//
+// There is one more thing left to do. Definitions are not a part of
+// the pure lambda calculus and aren't handled by our previous
+// functions. For the sake of simplicity, and to demonstrate that
+// definitions don't make the language stronger, we will simply
+// convert a program into an expression by turning each definition
+// into an expression that applies the body of the definition to a
+// lambda whose variable is the name of the definition and whose
+// body is the rest of the program. Definitions are just syntactic
+// sugar.
 
 static exp_type prg_to_exp(prg_type prg) {
   DATATYPE_switch
@@ -361,7 +370,7 @@ BRD_PARSER(static, prg_parse,
               *exp = Exp_Var(var); })))
 
 // The `BRD_PARSER' macro implements the simple parser generator.
-// The first two parameters of the macro specificy the linkage and
+// The first two parameters of the macro specify the linkage and
 // name of the main entry point to the generated parser. The next
 // two parameters specify what to do between tokens and how to match
 // punctuation tokens. The next parameter is a sequence of token
@@ -385,6 +394,18 @@ BRD_PARSER(static, prg_parse,
 //
 // ### Putting it all together
 
+static str_type eval(str_type code) {
+  str_type str = code;
+  prg_type prg = 0;
+  if (!prg_parse(&str, &prg))
+    ERROR_exit("Couldn't parse '%s'.", str);
+  str_skip_spaces(&str);
+  if ('\0' != *str)
+    ERROR_exit("Garbage '%s' following program.", str);
+
+  return exp_unparse(exp_reduce(exp_fresh(prg_to_exp(prg))));
+}
+
 int main(int argc, char* argv[]) {
   if (2 != argc) {
     fprintf
@@ -397,34 +418,28 @@ int main(int argc, char* argv[]) {
        "\n"
        "%s"
        "\n"
-       "For example,\n"
+       "For example, the program\n"
        "\n"
-       "  lambda 'def true = \\t.\\e.t\n"
-       "          def false = \\t.\\e.e\n"
-       "          ((true false) true)'\n"
+       "%s"
        "\n"
-       "would print\n"
+       "would evaluate to\n"
        "\n"
-       "  \\t.\\e.e\n",
-       lambda_syntax);
+       "  %s\n"
+       "\n"
+       "The numbers inside braces are produced by alpha conversion.\n",
+       lambda_syntax,
+       simple_example,
+       eval(simple_example));
     return EXIT_FAILURE;
   }
 
-  str_type str = argv[1];
-  prg_type prg = 0;
-  if (!prg_parse(&str, &prg))
-    ERROR_exit("Couldn't parse '%s'.", str);
-  str_skip_spaces(&str);
-  if ('\0' != *str)
-    ERROR_exit("Garbage '%s' following program.", str);
-
-  exp_type reduced_exp = exp_reduce(exp_fresh(prg_to_exp(prg)));
-  printf("%s\n", exp_unparse(reduced_exp));
+  printf("%s\n", eval(argv[1]));
 
   return EXIT_SUCCESS;
 }
 
 // ### Exercises
 //
-// 1. Change the parser to treat `def' as a keyword.
-// 2. Design and implement better error messages.
+// 1. Extend the parser to allow comments.
+// 2. Change the interpreter to use lazy evaluation.
+// 3. Design and implement better error handling and messages.
