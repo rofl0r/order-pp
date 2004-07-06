cfadfcb0966efb7b46b3c4a41862fff073af4386
vesa_karvonen <vesa_karvonen>
2004-07-06 17:10:16 +0000

    Separate header for variadic sequences

---
+++ inc/order/prelude/vseq.h
@@ -0,0 +1,10 @@
+#ifndef ORDER_PRELUDE_VSEQ_H_VAJK20040620
+#define ORDER_PRELUDE_VSEQ_H_VAJK20040620
+
+// (C) Copyright Vesa Karvonen 2004.
+//
+//    Distributed under the Boost Software License, Version 1.0.
+
+#define ORDER_PP_DEF_8vseq_to_seq ORDER_PP_FN_MACRO_P(1,ORDER_PP_SEQ_PARENTHESIZE)
+
+#endif
