c62c1fd09f6b32a4da1870084379a337cca51e02
vesa_karvonen <vesa_karvonen>
2004-07-26 20:17:37 +0000

    8call_cc

--- inc/order/prelude/control.h
+++ inc/order/prelude/control.h
@@ -5,11 +5,11 @@
 //
 //    Distributed under the Boost Software License, Version 1.0.
 
-// `8when(cond,body...)' is equivalent to `8if(cond,8do(body...),8nil)'.
-#define ORDER_PP_DEF_8when(cond,...) 8EVAL_IF,cond,8do(__VA_ARGS__),8nil,
-
-// `8unless(cond,body...)' is equivalent to `8if(cond,8nil,8do(body...))'.
-#define ORDER_PP_DEF_8unless(cond,...) 8EVAL_IF,cond,8nil,8do(__VA_ARGS__),
+// `8call_cc(f)' calls the unary function `f' with the current
+// continuation wrapped as a unary function.
+#define ORDER_PP_DEF_8call_cc ORDER_PP_FN_CM(1,8CALL_CC)
+#define ORDER_PP_8CALL_CC(P,f,...) (,(,8REPLACE_C,(,P##__VA_ARGS__)),ORDER_PP_OPEN f##P,P##__VA_ARGS__)
+#define ORDER_PP_8REPLACE_C(P,x,c,...) (,P##x,ORDER_PP_OPEN c##P)
 
 // `8exit(x)' exits the interpreter returning `x'.
 //
@@ -18,12 +18,6 @@
 //   8plus(1, 8exit(8nat_to_lit(2)), 3) ==> 2
 #define ORDER_PP_DEF_8exit ORDER_PP_FN_CM(1,8EXIT)
 #define ORDER_PP_8EXIT(P,x,...) ,x##P)
-
-#ifndef ORDER_PP_NDEBUG
-#define ORDER_PP_DEF_8with_assert(cond,body) ORDER_PP_MACRO(8if(cond,body,8exit(8quote(8Assert_Failed(8with_assert(cond,body))))))
-#else
-#define ORDER_PP_DEF_8with_assert(cond,body) ORDER_PP_MACRO(body)
-#endif
 
 // `8for_each_in_range(i,n,op)' calls the unary procedure `op' for
 // each natural number in the range $[i,i+n[$ in ascending order.
@@ -43,12 +37,24 @@
 #define ORDER_PP_8FOR_EACH_B(P,b,x,c,s,f,...) (,ORDER_PP_IF_NOT_##b(,,x##P,ORDER_PP_OPEN f##P,8FOR_EACH_C,x##P,c##P,s##P,f##P),P##__VA_ARGS__)
 #define ORDER_PP_8FOR_EACH_C(P,_,x,c,s,...) (,P##x,ORDER_PP_OPEN s##P,8FOR_EACH,P##c,P##s,P##__VA_ARGS__)
 
+// `8unless(cond,body...)' is equivalent to `8if(cond,8nil,8do(body...))'.
+#define ORDER_PP_DEF_8unless(cond,...) 8EVAL_IF,cond,8nil,8do(__VA_ARGS__),
+
 #define ORDER_PP_DEF_8until ORDER_PP_FN_CM(3,8UNTIL)
 #define ORDER_PP_8UNTIL(P,x,c,...) (,P##x,ORDER_PP_OPEN c##P,8UNTIL_B,P##x,P##c,P##__VA_ARGS__)
 #define ORDER_PP_8UNTIL_B(P,b,x,c,s,...) (,P##x,ORDER_PP_UNLESS_##b(,ORDER_PP_OPEN s##P,8UNTIL,P##c,P##s,)P##__VA_ARGS__)
 
+// `8when(cond,body...)' is equivalent to `8if(cond,8do(body...),8nil)'.
+#define ORDER_PP_DEF_8when(cond,...) 8EVAL_IF,cond,8do(__VA_ARGS__),8nil,
+
 #define ORDER_PP_DEF_8while ORDER_PP_FN_CM(3,8WHILE)
 #define ORDER_PP_8WHILE(P,x,c,...) (,P##x,ORDER_PP_OPEN c##P,8WHILE_B,P##x,P##c,P##__VA_ARGS__)
 #define ORDER_PP_8WHILE_B(P,b,x,c,s,...) (,P##x,ORDER_PP_WHEN_##b(,ORDER_PP_OPEN s##P,8WHILE,P##c,P##s,)P##__VA_ARGS__)
+
+#ifndef ORDER_PP_NDEBUG
+#define ORDER_PP_DEF_8with_assert(cond,body) ORDER_PP_MACRO(8if(cond,body,8exit(8quote(8Assert_Failed(8with_assert(cond,body))))))
+#else
+#define ORDER_PP_DEF_8with_assert(cond,body) ORDER_PP_MACRO(body)
+#endif
 
 #endif
