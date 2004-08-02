c2ede5ef737cb26c0add65a312ca397547d9f06d
vesa_karvonen <vesa_karvonen>
2004-08-02 20:46:59 +0000

    *** empty log message ***

--- inc/order/eval/rout.h
+++ inc/order/eval/rout.h
@@ -4,4 +4,4 @@
 
 #define ORDER_PP_DEF_8rout(...) 8EVAL_ROUT,(__VA_ARGS__),
 #define ORDER_PP_8EVAL_ROUT(P,e,p,G,...) (,P##e,ORDER_PP_DEF_8do P##p,8EVAL_ROUT_B,P##__VA_ARGS__)(9,)
-#define ORDER_PP_8EVAL_ROUT_B(P,x,...) (,P##x,P##__VA_ARGS__)(9,)
+#define ORDER_PP_8EVAL_ROUT_B(P,...) (,P##__VA_ARGS__)(9,)
