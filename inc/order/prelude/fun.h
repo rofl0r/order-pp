797eb1d3a9fdbc9be5ee0ff1d678b84626f52f0a
vesa_karvonen <vesa_karvonen>
2004-08-02 14:47:52 +0000

    *** empty log message ***

--- inc/order/prelude/fun.h
+++ inc/order/prelude/fun.h
@@ -6,7 +6,7 @@
 #define ORDER_PP_8CHAIN(P,f,g,...) (,(,ORDER_PP_OPEN f##P,ORDER_PP_OPEN g##P),P##__VA_ARGS__)
 
 #define ORDER_PP_DEF_8identity ORDER_PP_FN_CM(1,8IDENTITY)
-#define ORDER_PP_8IDENTITY(P,...) (,P##__VA_ARGS__)
+#define ORDER_PP_8IDENTITY
 
 #define ORDER_PP_DEF_8ignore ORDER_PP_FN_CM(1,8IGNORE)
 #define ORDER_PP_8IGNORE(P,x,...) (,,P##__VA_ARGS__)
