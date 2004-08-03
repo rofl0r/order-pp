e7e95c5ac6ad9a47ee6f73dcc14b4abb4f81beeb
vesa_karvonen <vesa_karvonen>
2004-08-03 18:58:35 +0000

    *** empty log message ***

--- inc/order/prelude/sym.h
+++ inc/order/prelude/sym.h
@@ -5,8 +5,8 @@
 #define ORDER_PP_DEF_8is_sym ORDER_PP_FN_NATIVE(1,9IS_SYM)
 #define ORDER_PP_9IS_SYM(P,x) ORDER_PP_IS_SYM(,P##x)(,8true,8false)
 
-#define ORDER_PP_DEF_8same ORDER_PP_FN_NATIVE(2,9SAME)
-#define ORDER_PP_9SAME(P,x,y) ORDER_PP_TEST(,ORDER_PP_SYM_##x##_##y(,),8true,8false)
-
 #define ORDER_PP_DEF_8not_same ORDER_PP_FN_NATIVE(2,9NOT_SAME)
 #define ORDER_PP_9NOT_SAME(P,x,y) ORDER_PP_TEST(,ORDER_PP_SYM_##x##_##y(,),8false,8true)
+
+#define ORDER_PP_DEF_8same ORDER_PP_FN_NATIVE(2,9SAME)
+#define ORDER_PP_9SAME(P,x,y) ORDER_PP_TEST(,ORDER_PP_SYM_##x##_##y(,),8true,8false)
