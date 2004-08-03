a4fc36284c50ef75bd17e8b73c1861ff694e6c91
vesa_karvonen <vesa_karvonen>
2004-08-03 17:59:37 +0000

    *** empty log message ***

--- inc/order/prelude/emit.h
+++ inc/order/prelude/emit.h
@@ -2,9 +2,6 @@
 //
 // Distributed under the Boost Software License, Version 1.0.
 
-#define ORDER_PP_DEF_8put ORDER_PP_FN_CM(1,8PUT)
-#define ORDER_PP_8PUT(P,x,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,1,P##x)
-
 #define ORDER_PP_DEF_8emit ORDER_PP_FN_CM(2,8EMIT)
 #define ORDER_PP_8EMIT(P,m,a,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,2,P##m,a##P)
 
@@ -14,14 +11,17 @@
 #define ORDER_PP_DEF_8emit_lparen ORDER_PP_FN_CM(1,8EMIT_LPAREN)
 #define ORDER_PP_8EMIT_LPAREN(P,x,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,4,P##x)
 
-#define ORDER_PP_DEF_8emit_rparen ORDER_PP_FN_CM(1,8EMIT_RPAREN)
-#define ORDER_PP_8EMIT_RPAREN(P,x,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,5,P##x)
-
 #define ORDER_PP_DEF_8emit_params ORDER_PP_FN_CM(3,8EMIT_PARAMS)
 #define ORDER_PP_8EMIT_PARAMS(P,i0,i1,p,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)ORDER_PP_NUM_BOP(,EQUAL,P##i0,P##i1)(,,(,2,ORDER_PP_ENUM_PARAMS,(,P##i0,P##i1,P##p)))
 #define ORDER_PP_ENUM_PARAMS(P,i0,i1,p) ORDER_PP_FX(OPEN,(ORDER_PP_ENUM_TRAILING_PARAMS(,P##i0,P##i1,P##p)))
+
+#define ORDER_PP_DEF_8emit_rparen ORDER_PP_FN_CM(1,8EMIT_RPAREN)
+#define ORDER_PP_8EMIT_RPAREN(P,x,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,5,P##x)
 
 #define ORDER_PP_DEF_8emit_trailing_params ORDER_PP_FN_CM(3,8EMIT_TRAILING_PARAMS)
 #define ORDER_PP_8EMIT_TRAILING_PARAMS(P,i0,i1,p,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,2,ORDER_PP_ENUM_TRAILING_PARAMS,(,P##i0,P##i1,P##p))
 #define ORDER_PP_ENUM_TRAILING_PARAMS(P,i0,i1,p) ORDER_PP_CM(P##i1,8FOR_EACH_IN_RANGE,P##i0,(,8EMIT_TRAILING_PARAM,P##p),8STOP)
 #define ORDER_PP_8EMIT_TRAILING_PARAM(P,i,p,K,...) ORDER_PP_##K(,,P##__VA_ARGS__),ORDER_PP_CAT(P##p,ORDER_PP_NUM_UOP(,TO_LIT,P##i))
+
+#define ORDER_PP_DEF_8put ORDER_PP_FN_CM(1,8PUT)
+#define ORDER_PP_8PUT(P,x,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)(,1,P##x)
--- inc/order/prelude/pp.h
+++ inc/order/prelude/pp.h
@@ -2,16 +2,16 @@
 //
 // Distributed under the Boost Software License, Version 1.0.
 
-#define ORDER_PP_DEF_8stringize ORDER_PP_FN_NATIVE(1,9STRINGIZE)
-#define ORDER_PP_9STRINGIZE(P,x) #x
+#define ORDER_PP_DEF_8adjacent ORDER_PP_FN_NATIVE(2,9ADJACENT)
+#define ORDER_PP_9ADJACENT(P,l,r) ORDER_PP_LEFT(,l##P)r##P
 
 #define ORDER_PP_DEF_8expand ORDER_PP_FN_NATIVE(2,9SEPARATE)
+
+#define ORDER_PP_DEF_8paste ORDER_PP_FN_NATIVE(2,9PASTE)
+#define ORDER_PP_9PASTE(P,l,r) l##r
 
 #define ORDER_PP_DEF_8separate ORDER_PP_FN_NATIVE(2,9SEPARATE)
 #define ORDER_PP_9SEPARATE(P,l,r) l##P P##r
 
-#define ORDER_PP_DEF_8adjacent ORDER_PP_FN_NATIVE(2,9ADJACENT)
-#define ORDER_PP_9ADJACENT(P,l,r) ORDER_PP_LEFT(,l##P)r##P
-
-#define ORDER_PP_DEF_8paste ORDER_PP_FN_NATIVE(2,9PASTE)
-#define ORDER_PP_9PASTE(P,l,r) l##r
+#define ORDER_PP_DEF_8stringize ORDER_PP_FN_NATIVE(1,9STRINGIZE)
+#define ORDER_PP_9STRINGIZE(P,x) #x
