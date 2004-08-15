c26b08ad1be622da029839dc875d51eb7c66dc4f
vesa_karvonen <vesa_karvonen>
2004-08-15 13:31:36 +0000

    *** empty log message ***

--- inc/order/eval/cond.h
+++ inc/order/eval/cond.h
@@ -6,8 +6,13 @@
 
 #define ORDER_PP_EVAL_COND_SPLIT(P,c,...) P##c,ORDER_PP_IS_TUPLE_SIZE_1(,P##__VA_ARGS__)(,ORDER_PP_REM,8do)(P##__VA_ARGS__),
 
+#ifdef ORDER_PP_DEBUG
+#define ORDER_PP_8EVAL_COND(P,e,bt,ct,ts,G,...) ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##bt())(,P##e,ORDER_PP_DEF_##bt,8EVAL_COND_LOOP,P##ct,ORDER_PP_EVAL_COND_SPLIT P##ts,P##e,P##__VA_ARGS__)
+#define ORDER_PP_8EVAL_COND_LOOP(P,b0,ct0,bt1,ct1,ts,e,...) ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##ct0())ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##bt1())(,P##e,ORDER_PP_IF_##b0(ORDER_PP_DEF_,P##ct0,P##bt1,8EVAL_COND_LOOP,P##ct1,ORDER_PP_EVAL_COND_SPLIT P##ts,P##e),P##__VA_ARGS__)
+#else
 #define ORDER_PP_8EVAL_COND(P,e,bt,ct,ts,G,...) (,P##e,ORDER_PP_DEF_##bt,8EVAL_COND_LOOP,P##ct,ORDER_PP_EVAL_COND_SPLIT P##ts,P##e,P##__VA_ARGS__)
 #define ORDER_PP_8EVAL_COND_LOOP(P,b0,ct0,bt1,ct1,ts,e,...) (,P##e,ORDER_PP_IF_##b0(ORDER_PP_DEF_,P##ct0,P##bt1,8EVAL_COND_LOOP,P##ct1,ORDER_PP_EVAL_COND_SPLIT P##ts,P##e),P##__VA_ARGS__)
+#endif
 
 #define ORDER_PP_DEF_0cond 0EVAL_COND,
 #define ORDER_PP_0EVAL_COND(P,e0,G,eval_cond_loop,ct,ts,e1,K,...) ORDER_PP_##K(,,P##__VA_ARGS__)
