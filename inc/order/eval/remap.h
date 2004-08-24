e4cc4ebf6f3c40b8d56aeab76c9ec2705b38b96a
vesa_karvonen <vesa_karvonen>
2004-08-24 19:06:36 +0000

    *** empty log message ***

--- inc/order/eval/remap.h
+++ inc/order/eval/remap.h
@@ -3,17 +3,17 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #ifdef ORDER_PP_DEBUG
-#define ORDER_PP_DEF_8remap(t,...) ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##t())ORDER_PP_DEF_##t,8EVAL_REMAP,ORDER_PP_IS_TUPLE_SIZE_1(,0##__VA_ARGS__)(ORDER_PP_REMAP_,NONE,SOME)(,ORDER_PP_LIT_DEC_##__VA_ARGS__),
+#define ORDER_PP_DEF_8remap(t,...) ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##t())ORDER_PP_DEF_##t,8EVAL_REMAP,ORDER_PP_IS_TUPLE_SIZE_1(,0##__VA_ARGS__)(ORDER_PP_REMAP_,NONE,SOME)(,__VA_ARGS__),
 #else
-#define ORDER_PP_DEF_8remap(t,...) ORDER_PP_DEF_##t,8EVAL_REMAP,ORDER_PP_IS_TUPLE_SIZE_1(,0##__VA_ARGS__)(ORDER_PP_REMAP_,NONE,SOME)(,ORDER_PP_LIT_DEC_##__VA_ARGS__),
+#define ORDER_PP_DEF_8remap(t,...) ORDER_PP_DEF_##t,8EVAL_REMAP,ORDER_PP_IS_TUPLE_SIZE_1(,0##__VA_ARGS__)(ORDER_PP_REMAP_,NONE,SOME)(,__VA_ARGS__),
 #endif
 
 #define ORDER_PP_REMAP_NONE(P,n) P##n,(,)
 #define ORDER_PP_REMAP_SOME(P,n,...) P##n,(,P##__VA_ARGS__,)
 
-#define ORDER_PP_8EVAL_REMAP(P,f,n,is,G,...) (,(,8REMAP_FST,P##f,P##n,P##is),P##__VA_ARGS__)
-#define ORDER_PP_8REMAP_FST(P,v,f,n,is,...) (,ORDER_PP_LIT_IS_0(,P##n)(,ORDER_PP_REMAP,)(,8REMAP_RST,P##f,ORDER_PP_LIT_DEC_##n,P##is,(,P##v)),P##__VA_ARGS__)
-#define ORDER_PP_8REMAP_RST(P,v,f,n,is,vs,...) (,ORDER_PP_LIT_IS_0(,P##n)(,ORDER_PP_REMAP,)(,8REMAP_RST,P##f,ORDER_PP_LIT_DEC_##n,P##is,(,ORDER_PP_RIGHT vs##P,P##v)),P##__VA_ARGS__)
+#define ORDER_PP_8EVAL_REMAP(P,f,n,is,G,...) (,(,8REMAP_FST,P##f,ORDER_PP_LIT_DEC(,P##n),P##is),P##__VA_ARGS__)
+#define ORDER_PP_8REMAP_FST(P,v,f,n,is,...) (,ORDER_PP_LIT_IS_0(,P##n)(,ORDER_PP_REMAP,)(,8REMAP_RST,P##f,ORDER_PP_LIT_DEC(,P##n),P##is,(,P##v)),P##__VA_ARGS__)
+#define ORDER_PP_8REMAP_RST(P,v,f,n,is,vs,...) (,ORDER_PP_LIT_IS_0(,P##n)(,ORDER_PP_REMAP,)(,8REMAP_RST,P##f,ORDER_PP_LIT_DEC(,P##n),P##is,(,ORDER_PP_RIGHT vs##P,P##v)),P##__VA_ARGS__)
 
 #define ORDER_PP_REMAP(P,remap_rst,f,n,is,vs) P##f ORDER_PP_FX(PSEQ_TO_APS_A,ORDER_PP_FX(PSEQ_REVERSE,(,ORDER_PP_PM(,(,ORDER_PP_OPEN vs##P,),8REMAP,ORDER_PP_OPEN is##P,,))))(0,)
 
