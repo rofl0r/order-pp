6d09031c4fc3f85d042c76a32a184153e5e4fa40
vesa_karvonen <vesa_karvonen>
2004-08-29 21:59:09 +0000

    *** empty log message ***

--- example/is_function.cpp
+++ example/is_function.cpp
@@ -3,3 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #include "is_function.hpp"
+
+int main() {
+  return 0;
+}
--- example/is_integral.cpp
+++ example/is_integral.cpp
@@ -3,3 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #include "is_integral.hpp"
+
+int main() {
+  return 0;
+}
