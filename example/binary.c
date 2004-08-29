ebcf3948b12975bca7775ffecd66cfd55326d01e
vesa_karvonen <vesa_karvonen>
2004-08-29 17:02:56 +0000

    *** empty log message ***

--- example/binary.c
+++ example/binary.c
@@ -7,24 +7,52 @@
 
 // ### Test
 //
-// The last thing to do is to test our macro. The following `main'
-// program is just a simple test of the `BINARY' macro.
+// The last thing to do is to test our macro. Unlike in most of the
+// examples, the `BINARY' macro implementation uses many hand
+// written token extraction macros that might contain errors. In
+// order to test all the token extraction macros, we could manually
+// write a corresponding test for each macro, but a better way is to
+// generate the tests, which reduces the chance of having an
+// undetected typo.
+//
+// First we'll implement an Order function to convert a number
+// to a sequence of binary digits.
 
-#define CHECK(actual, expected)                         \
-printf(actual == expected                               \
-       ? #actual " == " #expected " -- OK.\n"           \
-       : #actual " != " #expected " -- ERROR!\n")
+#define ORDER_PP_DEF_8num_to_seq_of_binary_digits               \
+ORDER_PP_FN(8fn(8N,                                             \
+                8if(8is_0(8N),                                  \
+                    8seq(0),                                    \
+                    8seq_reverse(8seq_unfold                    \
+                                 (8isnt_0,                      \
+                                  8ap(8flip(8quotient), 2),     \
+                                  8ap(8flip(8remainder), 2),    \
+                                  8N)))))
+
+// We'll also use an ad hoc code generation macro to generate a
+// single test case.
+
+#define CHECK(digits, value)                         \
+printf(BINARY(digits) == value                       \
+       ? #digits " == " #value " -- OK.\n"           \
+       : #digits " != " #value " -- ERROR!\n");
+
+// We are then ready to start implementing the tests. We will use
+// the Order interpreter to generate invocations of the above
+// `CHECK' macro for all integers in the range `[0,15]', which is
+// enough to cover all the token extraction macros we defined. We'll
+// also write one simple sanity check that contains multiple tokens.
+// These test cases should give a fairly good coverage.
 
 int main() {
-  CHECK(BINARY(0), 0);
-  CHECK(BINARY(1), 1);
 
-  CHECK(BINARY(0 0), BINARY(00));
-  CHECK(BINARY(0 1), BINARY(01));
-  CHECK(BINARY(1 0), BINARY(10));
-  CHECK(BINARY(1 1), BINARY(11));
+  ORDER_PP(8for_each_in_range
+           (8chain(8emit(8(CHECK)),
+                   8fn(8N,
+                       8tuple(8seq_cat(8num_to_seq_of_binary_digits(8N)),
+                              8to_lit(8N)))),
+            0, 15))
 
-  CHECK(BINARY(1 01 010 0010 11), 0xA8B);
+  CHECK(1 01 010 0010 11, 0xA8B)
 
   return 0;
 }
