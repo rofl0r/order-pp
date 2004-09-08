a9157f13173dcd858b6205aa8109b6299ee2302c
vesa_karvonen <vesa_karvonen>
2004-09-08 15:51:39 +0000

    *** empty log message ***

--- inc/order/prelude/sym.h
+++ inc/order/prelude/sym.h
@@ -3,15 +3,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #define ORDER_PP_DEF_8is_sym ORDER_PP_FN_CM(1,8IS_SYM,0IS_ANY)
-#define ORDER_PP_8IS_SYM(P,x,...) (,ORDER_PP_IS_SYM(,P##x)(,8true,8false),P##__VA_ARGS__)
+#define ORDER_PP_8IS_SYM(P,x,...) (,ORDER_PP_0IS_SYM(,P##x)(,8true,8false),P##__VA_ARGS__)
 
 #define ORDER_PP_DEF_8not_same ORDER_PP_FN_NATIVE(2,9NOT_SAME,0IS_SYM,0IS_SYM)
 #define ORDER_PP_9NOT_SAME(P,x,y) ORDER_PP_NOT_SAME(,P##x,P##y)(,8true,8false)
 
 #define ORDER_PP_DEF_8same ORDER_PP_FN_NATIVE(2,9SAME,0IS_SYM,0IS_SYM)
 #define ORDER_PP_9SAME(P,x,y) ORDER_PP_SAME(,P##x,P##y)(,8true,8false)
-
-// Detail
-
-// TBD: 0IS_SYM
-#define ORDER_PP_0IS_SYM(P,x) ORDER_PP_IF_8true
