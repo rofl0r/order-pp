327b98ac324cb1aa2b8de6b8b666f9bd83ee948c
vesa_karvonen <vesa_karvonen>
2004-09-08 16:02:25 +0000

    *** empty log message ***

--- inc/order/prelude/boolean.h
+++ inc/order/prelude/boolean.h
@@ -33,6 +33,4 @@
 
 // Detail
 
-// TBD: 0IS_BOOL
-#define ORDER_PP_0IS_BOOL(P,x) ORDER_PP_IF_8true
-
+#define ORDER_PP_0IS_BOOL(P,x) ORDER_PP_AND(not)(ORDER_PP_IS_EDIBLE(,P##x))(and)(ORDER_PP_OR(or)(ORDER_PP_SAME(,8true,P##x))(or)(ORDER_PP_SAME(,8false,P##x))())()
