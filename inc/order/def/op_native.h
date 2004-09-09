514efcc75f461a47b38435fb42b3a78c7d0715bd
vesa_karvonen <vesa_karvonen>
2004-09-09 23:10:16 +0000

    *** empty log message ***

--- inc/order/def/op_native.h
+++ inc/order/def/op_native.h
@@ -6,7 +6,7 @@
 # define ORDER_PP_OP_LEFT_NATIVE(m,p) 8DEF_OP_LN,m,p,ORDER_PP_OP_LN_PARAMS
 # define ORDER_PP_OP_LN_PARAMS(...) (,__VA_ARGS__,0op_ln),
 
-# define ORDER_PP_8DEF_OP_LN(P,e,m,p,ts,G,...) (,P##e,ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_FIRST ts##P())ORDER_PP_DEF_FIRST ts##P,8DEF_OP_LN_B,e##P,m##P,p##P,ORDER_PP_TUPLE_REST ts##P,P##__VA_ARGS__)
+# define ORDER_PP_8DEF_OP_LN(P,e,m,p,ts,G,...) (,P##e,ORDER_PP_IS_EDIBLE(,P##ts)(,,8EXIT_SYNTAX_ERROR,P##m,)ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_FIRST ts##P())ORDER_PP_DEF_FIRST ts##P,8DEF_OP_LN_B,e##P,m##P,p##P,ORDER_PP_TUPLE_REST ts##P,P##__VA_ARGS__)
 # define ORDER_PP_8DEF_OP_LN_B(P,v,e,m,p,t,...) (,ORDER_PP_TYPE_CHECK(,P##p,P##v,)P##e,ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##t)ORDER_PP_DEF_##t,8DEF_OP_LN_C,e##P,P##m,P##p,P##v,P##__VA_ARGS__)
 # define ORDER_PP_8DEF_OP_LN_C(P,vr,e,m,p,vl,t,...) (,ORDER_PP_TYPE_CHECK(,P##p,P##vr,)P##e,ORDER_PP_ASSERT_SYNTAX(ORDER_PP_DEF_##t())ORDER_PP_DEF_##t,8DEF_OP_LN_C,e##P,P##m,P##p,ORDER_PP_##p(,P##vr)(,ORDER_PP_##m,)(,P##vl,P##vr),P##__VA_ARGS__)
 
