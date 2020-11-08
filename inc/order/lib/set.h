49af9c553b9c7fa4f52b7812f3fcc7246537a13b
Mark Johnson <foundry@protonmail.com>
2020-11-08 11:37:16 -0500

    make 8set_items return proper sequence; add placemarker to some __VA_ARGS__ that were missing it

--- inc/order/lib/set.h
+++ inc/order/lib/set.h
@@ -9,7 +9,6 @@
 // First-order
 
 #define ORDER_PP_DEF_8set(cmp, ...) 8EVAL_SET,cmp,ORDER_PP_SEQ_AT_0 __VA_ARGS__(0set_eval_terminate),
-
 #define ORDER_PP_8EVAL_SET(P, env, q_cmp, q_head, tail, G, ...) \
                           (,P##env,ORDER_PP_DEF_##q_cmp,8EVAL_SET_LOOP,P##q_head,P##tail,,P##env,P##__VA_ARGS__)
 #define ORDER_PP_8EVAL_SET_LOOP(P, cmp, q_head, tail, acc, env, ...) \
@@ -19,15 +18,14 @@
 
 #define ORDER_PP_DEF_0set_eval_terminate 0EVAL_SET_TERMINATE,
 #define ORDER_PP_0EVAL_SET_TERMINATE(P, env, G, _eval_set_loop_b_, _tail_, _acc_, _cmp_, _env_,...) \
-                                    (,(P##_cmp_,ORDER_PP_9VSEQ_TO_SEQ_OF_TUPLES(,P##_acc_)), __VA_ARGS__)   
+                                    (,(P##_cmp_,ORDER_PP_9VSEQ_TO_SEQ_OF_TUPLES(,P##_acc_)), P##__VA_ARGS__)   
 
 
-#define ORDER_PP_DEF_8set_equivalence_fn ORDER_PP_FN_CM(1,8SET_EQUIVALENCE_FN,0IS_SET)
-#define ORDER_PP_8SET_EQUIVALENCE_FN(P, set, ...) (,ORDER_PP_FX(TUPLE_AT_0,(,ORDER_PP_REM P##set,)),__VA_ARGS__)
+#define ORDER_PP_DEF_8set_equivalence_fn ORDER_PP_FN_NATIVE(1,8SET_EQUIVALENCE_FN,0IS_SET)
+#define ORDER_PP_8SET_EQUIVALENCE_FN(P, set) ORDER_PP_FX(TUPLE_AT_0,(,ORDER_PP_REM P##set,))
 
 
-#define ORDER_PP_DEF_8set_items ORDER_PP_FN_CM(1,8SET_ITEMS,0IS_SET)
-#define ORDER_PP_8SET_ITEMS(P, set, ...) (,ORDER_PP_FX(TUPLE_AT_1,(,ORDER_PP_REM P##set,)),__VA_ARGS__)
+#define ORDER_PP_DEF_8set_items(set) ORDER_PP_MACRO(8seq_map(8tuple_at_0, 8map_items(set)))
 
 
 #define ORDER_PP_DEF_8set_exists ORDER_PP_MACRO(8map_exists)
@@ -39,8 +37,8 @@
 #define ORDER_PP_DEF_8set_erase ORDER_PP_MACRO(8map_erase)
 
 
-#define ORDER_PP_DEF_8is_set ORDER_PP_FN_CM(1,8IS_SET,0IS_ANY)
-#define ORDER_PP_8IS_SET(P,x,...) (,ORDER_PP_0IS_SET(,P##x)(,8true,8false),P##__VA_ARGS__)
+#define ORDER_PP_DEF_8is_set ORDER_PP_FN_NATIVE(1,8IS_SET,0IS_ANY)
+#define ORDER_PP_8IS_SET(P,x) ORDER_PP_0IS_SET(,P##x)(,8true,8false)
 
 
 // Detail
